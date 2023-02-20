519c42c8e536d8765d97a62bbfb10fb9a8d374ab
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2023-02-20 15:16:28 +0530

    server initialised

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from 'cors';
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+app.use(cors())
+
+
+const CONNECTION_URL = process.env.CONNECTION_URL || "mongodb://localhost:27017/tiffinservice?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running at PORT ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+
